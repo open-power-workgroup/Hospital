b194eb098b433db7c3c96049efbd2ca1aa32c426
Zhang Cheng <13501393281@163.com>
2016-05-05 13:08:18 -0400

    add debug mode

--- resource/code_resource_python/readme_reader.py
+++ resource/code_resource_python/readme_reader.py
@@ -24,7 +24,10 @@ the dictionary will of this form:
 
 }
 """
+import json
+import os
 import re
+
 try:
     import helpers
 except ImportError:
@@ -81,7 +84,7 @@ def __get_info_line_num__(content_lines=('',)):
     return forward_index, backward_index
 
 
-def __parse_all__(content_lines=("",)):
+def __parse_all__(content_lines=("",), debug_mode=False):
     # init variable
     prev_leading_space_num = 0  # this is the number of leading white space in the previous line
     prev_type_index = 0  # this is the type index of the previous line(see helpers.TYPE_MAP for more)
@@ -93,14 +96,25 @@ def __parse_all__(content_lines=("",)):
     for content_line in content_lines:
         if not content_line.isspace():  # if this line is not space
 
+            if debug_mode:
+                helpers.write_log('')
+                helpers.write_log('this is the current line we are processing:')
+                helpers.write_log(content_line)
+
             # it is a title, which means this is a city
             if content_line.startswith('#'):
                 parsed_line = __parse_city__(content_line)
+                if debug_mode:
+                    helpers.write_log('this is a city line')
                 if parsed_line == '':
                     print('we encounter a warning while parsing readme: ')
                     print('this line will be parsed as empty:')
                     print(content_line[:-1])
                     print('we will not add this line to the result')
+                    if debug_mode:
+                        helpers.write_log('WARNING: this line contains illegal string, that will parse as empty')
+                    else:
+                        print('you can run program in debug mode for further information')
                 else:
                     info_dict.update({parsed_line: {}})
                     cur_city = parsed_line
@@ -123,6 +137,10 @@ def __parse_all__(content_lines=("",)):
                         print('this line will be parsed as empty:')
                         print('"' + content_line[:-1] + '"')
                         print('we will not add this line to the result')
+                        if debug_mode:
+                            helpers.write_log('WARNING: this line contains illegal string, that will parse as empty')
+                        else:
+                            print('you can run program in debug mode for further information')
                     else:
                         info_dict[cur_city].update({parsed_line: {}})
                         cur_hospital = parsed_line
@@ -136,6 +154,11 @@ def __parse_all__(content_lines=("",)):
                             print('"' + content_line[:-1] + '"')
                             print('the hospital is', cur_hospital)
                             print('the city is', cur_city)
+                            if debug_mode:
+                                helpers.write_log(
+                                    'WARNING: this line is a duplicate of info')
+                            else:
+                                print('you can run program in debug mode for further information')
                         else:  # key in info dict and value not duplicated
                             info_dict[cur_city][cur_hospital][key].append(value)
                     except KeyError:  # key not in info dict
@@ -148,12 +171,26 @@ def __parse_all__(content_lines=("",)):
                     print('    previous line has', prev_leading_space_num, 'leading white spaces')
                     print('    current line has', cur_leading_space_num, 'leading white spaces')
                     input('press <enter> to continue')
+                    if debug_mode:
+                        helpers.write_log('we cannot recognize the type of the line')
+                        helpers.write_log('here is some information you can send to the issue:')
+                        helpers.write_log('    this is the line')
+                        helpers.write_log('   ', "'" + content_line[:-1] + "'")
+                        helpers.write_log('    previous line has', prev_leading_space_num, 'leading white spaces')
+                        helpers.write_log('    current line has', cur_leading_space_num, 'leading white spaces')
 
                 # update all the previous variable for the next round
                 prev_type_index = cur_type_index
                 prev_leading_space_num = cur_leading_space_num
+                if debug_mode:
+                    helpers.write_log('the current line has', cur_leading_space_num, 'leading white space')
+                    helpers.write_log('the current line has type index of', cur_type_index)
 
     print('parse all finished')
+    if debug_mode:
+        helpers.write_log()
+        helpers.write_log('here is a json of the output: ')
+        helpers.write_log(json.dumps(info_dict, ensure_ascii=False, sort_keys=True, indent=4))
     return info_dict
 
 
@@ -161,6 +198,25 @@ def read():
     lines = helpers.read_file('README.md')
     start, end = __get_info_line_num__(lines)
     info_dict = __parse_all__(lines[start:end])
+    return info_dict
+
+
+def debug():
+    # try to remove debug file
+    try:
+        os.remove('debug.log')
+    except:
+        pass
+    lines = helpers.read_file('README.md')
+    print('reading file finished')
+    start, end = __get_info_line_num__(lines)
+    print()
+    print('got the start and end position of hospital list')
+    print('start is :', start)
+    print('end is:', end)
+    print('parsing started, this can take a while, please wait')
+    info_dict = __parse_all__(lines[start:end], debug_mode=True)
+    print('debug finished, see the log on debug.log')
     return info_dict
 
 
