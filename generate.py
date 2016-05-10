5b24225f19a4a172b2cc72f5339b5bbe780ac353
xokctah <xokctah@gmail.com>
2016-05-10 16:00:34 +0800

    specify dataversion to generate

--- generate.py
+++ generate.py
@@ -1,42 +1,12 @@
+import os
+import sys
+
 import yaml
 import json
-import os
 
 from jinja2 import Template
 from pypinyin import lazy_pinyin
 
-
-hospitals = {}
-
-for filename in os.listdir('data'):
-    if filename.endswith('.yml') and (not filename.startswith('example')):
-        file = 'data/' + filename
-        print('Loading - ' + file)
-        with open(file, 'r', encoding='utf-8') as stream:
-            hospital = yaml.load(stream)
-            province = hospital['province']
-            name = hospital['name']
-            if province not in hospitals:
-                hospitals[province] = {}
-            hospitals[province][name] = hospital
-
-# dump to json files
-dist = 'output/json'
-os.makedirs(dist, exist_ok=True)
-
-provinces = {}
-for province in hospitals:
-    province_pinyin = "".join(lazy_pinyin(province))
-    province_filename = province_pinyin + '.json'
-    provinces[province] = {'json_filename': province_filename, 'pinyin': province_pinyin}
-    json.dump(hospitals[province], open(dist + '/'+ province_filename, 'w'), ensure_ascii=False, indent=2)
-
-json.dump(provinces, open(dist + '/hospitals.json', 'w'), ensure_ascii=False, indent=2)
-
-# dump to HTML file
-template_dir = 'resource/templates'
-dist_dir = 'output/html'
-os.makedirs(dist, exist_ok=True)
 
 def render_html(template_file, dist_file, **kwargs):
     with open(template_file, 'r') as f:
@@ -45,7 +15,52 @@ def render_html(template_file, dist_file, **kwargs):
         with open(dist_file, 'w') as f:
             f.write(html)
 
-render_html(template_dir + '/hospitals.phtml', dist_dir + '/hospitals.html', provinces=provinces)
 
-for province in hospitals:
-    render_html(template_dir + '/province.phtml', dist_dir + '/' + provinces[province]['pinyin'] + '.html', hospitals=hospitals[province])
+def do_generate(version):
+    hospitals = {}
+
+    for filename in os.listdir('data'):
+        if filename.endswith('.yml') and (not filename.startswith('example')):
+            file = 'data/' + filename
+            print('Loading - ' + file)
+            with open(file, 'r', encoding='utf-8') as stream:
+                hospital = yaml.load(stream)
+                if hospital['dataversion'] != version:
+                    continue
+
+                province = hospital['province']
+                name = hospital['name']
+                if province not in hospitals:
+                    hospitals[province] = {}
+                hospitals[province][name] = hospital
+
+    # dump to json files
+    dist = 'output/json'
+    os.makedirs(dist, exist_ok=True)
+
+    provinces = {}
+    for province in hospitals:
+        province_pinyin = "".join(lazy_pinyin(province))
+        province_filename = province_pinyin + '.json'
+        provinces[province] = {'json_filename': province_filename, 'pinyin': province_pinyin}
+        json.dump(hospitals[province], open(dist + '/'+ province_filename, 'w'), ensure_ascii=False, indent=2)
+
+    json.dump(provinces, open(dist + '/hospitals.json', 'w'), ensure_ascii=False, indent=2)
+
+    # dump to HTML file
+    template_dir = 'resource/templates'
+    dist_dir = 'output/html'
+    os.makedirs(dist, exist_ok=True)
+
+    render_html(template_dir + '/hospitals.phtml', dist_dir + '/hospitals.html', provinces=provinces)
+
+    for province in hospitals:
+        render_html(template_dir + '/province.phtml', dist_dir + '/' + provinces[province]['pinyin'] + '.html', hospitals=hospitals[province])
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('USAGE: python generate.py [data version]')
+    else:
+        do_generate(sys.argv[1])
