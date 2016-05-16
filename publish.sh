136ba317961ccdc44cf4a73b383df26ce5803f9b
xokctah <xokctah@gmail.com>
2016-05-16 13:55:19 +0800

    publish.sh should not be here

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# run this script AFTER "python generate.py [data version]"
-git checkout gh-pages && \
-rm -rf data/* && \
-mv output/* data/ && \
-git commit -am "update published website and json api" && \
-git push && \
-git checkout incoming
