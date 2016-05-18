dd80043d18207bf15d1aa77f96ce12abdb51eb2e
xokctah <xokctah@gmail.com>
2016-05-18 21:21:50 +0800

    minor adjustment to publish script

--- publish.sh
+++ publish.sh
@@ -4,7 +4,9 @@
 git checkout gh-pages && \
 rm -rf data/* && \
 mv output/* data/ && \
+git add . && \
 git commit -am "update published website and json api" && \
+git pull --rebase && \
 git push && \
 git checkout master
 
